e38fa95af6357985c432f75b40ebe02b4099e882
Sanjay <sanjaykeloth321@gmail.com>
2024-10-23 19:29:09 +0530

    chore: created controller and services

---
+++ src/episodes/episodes.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { Episode } from './entity/episode.entity';
+import { randomUUID } from 'crypto';
+import { CreateEpisodeDto } from './dto/create-episode.dto';
+
+@Injectable()
+export class EpisodesService {
+  private episodes: Episode[] = [];
+
+  async findAll(sort: 'asc' | 'desc' = 'asc') {
+    const sortAsc = (a: Episode, b: Episode) => (a.name > b.name ? 1 : -1);
+    const sortDesc = (a: Episode, b: Episode) => (a.name < b.name ? 1 : -1);
+
+    return sort === 'asc'
+      ? this.episodes.sort(sortAsc)
+      : this.episodes.sort(sortDesc);
+  }
+
+  async findFeatured() {
+    return this.episodes.filter((episode) => episode.featured);
+  }
+
+  async findOne(id: string) {
+    return this.episodes.filter((episode) => episode.featured);
+  }
+
+  async create(createEpisodeDto: CreateEpisodeDto) {
+    const newEpisode = { ...createEpisodeDto, id: randomUUID() };
+    this.episodes.push(newEpisode);
+
+    return newEpisode;
+  }
+}
