80d520c33eea60e50332d5cec6e2defaad677b17
Sanjay <sanjaykeloth321@gmail.com>
2024-11-04 23:05:49 +0530

    chore: add imports in tests

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { ConfigModule } from 'src/config/config.module';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [EpisodesController],
+      providers: [EpisodesController],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
